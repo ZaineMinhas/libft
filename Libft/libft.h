fd53fc1a90f542b8d8e104f60531a10a3740e991
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-11-19 12:05:23 +0100

    mise a jour des exos

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 14:53:21 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/18 14:59:01 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/11/19 11:56:45 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 # define LIBFT_H
 
 # include <unistd.h>
+
+int		ft_atoi(const char *str);
+void	ft_bzero(void *s, size_t n);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*memchr(const void *s, int c, size_t n);
+int		memcmp(const void *s1, const void *s2);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+char	*strchr(const char *s, int c);
+size_t	ft_strlcat(char *dest, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strrchr(const char *s, int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
