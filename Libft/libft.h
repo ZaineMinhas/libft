c652fe3bdf37d202a773978415db9cd1d520405d
Zaine-el-abidine Minhas <zminhas@mi-r3-p1.s19.be>
2020-11-23 20:05:51 +0100

    GROS COMMIT POUR GROS PUSH

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 14:53:21 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/23 17:31:09 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/11/23 19:53:31 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,5 +47,9 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
