a639458c22f2a9e6f098fd1a97edd88f308d1572
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-11-26 17:47:14 +0100

    mise a jour des exos

---
+++ Libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/26 16:15:07 by zminhas           #+#    #+#             */
+/*   Updated: 2020/11/26 17:46:54 by zminhas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	**dest;
+	t_list	*new;
+
+	if (!lst)
+		return (NULL);
+	dest = NULL;
+	if (!(*dest = malloc(sizeof(t_list) * ft_lstsize(lst))))
+		return (NULL);
+	while ((*dest)->next)
+	{
+		if (!(new = ft_lstnew(f(lst->content))))
+		{
+			ft_lstdelone(lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(dest, new);
+	}
+	return (*dest);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 14:53:21 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/26 13:43:23 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/11/26 16:17:33 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,5 +68,6 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void*));
 void			ft_lstclear(t_list **lst, void (*del)(void*));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
