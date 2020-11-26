bc82cea83aa43531e07ae21f2830c9d66a55bd26
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-11-26 18:20:25 +0100

    mise a jour des exos

--- Libft/ft_lstmap_bonus.c
+++ Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:15:07 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/26 17:46:54 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/11/26 18:09:05 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	**dest;
+	t_list	*dest;
 	t_list	*new;
 
 	if (!lst)
 		return (NULL);
 	dest = NULL;
-	if (!(*dest = malloc(sizeof(t_list) * ft_lstsize(lst))))
+	if (!(dest = malloc(sizeof(t_list) * ft_lstsize(lst))))
 		return (NULL);
-	while ((*dest)->next)
+	while (dest->next)
 	{
 		if (!(new = ft_lstnew(f(lst->content))))
 		{
 			ft_lstdelone(lst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(dest, new);
+		ft_lstadd_back(&dest, new);
 	}
-	return (*dest);
+	return (dest);
 }
