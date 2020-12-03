0c88ce504c5bef877354a1e95763d5486af03226
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-12-03 14:12:06 +0100

    mise a jour des exos

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 11:51:45 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/18 11:56:07 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/12/03 14:07:31 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen(const char *s)
 {
-	int i;
+	size_t i;
 
 	i = 0;
 	while (s[i])
