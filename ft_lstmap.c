ee15b0ba4c8c0699833d73a291d235dca1152252
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-12-09 16:06:37 +0100

    mise a jour des exos

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,34 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zminhas <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:15:07 by zminhas           #+#    #+#             */
-/*   Updated: 2020/11/27 15:33:36 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/12/09 16:02:39 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static	void	ft_free_lst(t_list	*lst_start)
+{
+	ft_lstiter(lst_start, free);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del)(void *))
 {
 	t_list	*dest;
 	t_list	*elemf;
+	t_list	*lst_start;
 
 	if (!lst)
 		return (NULL);
 	if (!f)
 		return (lst);
 	dest = NULL;
+	lst_start = lst;
 	while (lst)
 	{
 		if (!(elemf = ft_lstnew(f(lst->content))))
 		{
-			ft_lstclear(&elemf, del);
+			if (del)
+				ft_lstclear(&elemf, del);
+			else
+				ft_free_lst(lst_start);
 			return (NULL);
 		}
 		ft_lstadd_back(&dest, elemf);
 		lst = lst->next;
 	}
+	free(lst_start);
 	return (dest);
 }
