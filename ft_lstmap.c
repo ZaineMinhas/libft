0c6b543b01c1b0e04ce3b4b32484abdddbe30f2c
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-12-12 13:32:36 +0100

    mise a jour des exos

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,18 +6,18 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:15:07 by zminhas           #+#    #+#             */
-/*   Updated: 2020/12/09 16:02:39 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/12/12 13:32:06 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	void	ft_free_lst(t_list	*lst_start)
+static void	ft_free_lst(t_list *lst_start)
 {
 	ft_lstiter(lst_start, free);
 }
 
-t_list	*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del)(void *))
+t_list		*ft_lstmap(t_list *lst, void *(*f) (void *), void (*del)(void *))
 {
 	t_list	*dest;
 	t_list	*elemf;
