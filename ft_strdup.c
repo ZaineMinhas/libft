ecb1e0cf363f85bcd5f88da4e270cfd470f3bec5
Zaine-el-abidine Minhas <zminhas@mi-r2-p1.s19.be>
2020-12-14 17:31:05 +0100

    amelioration du strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: zminhas <zminhas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 14:51:19 by zminhas           #+#    #+#             */
-/*   Updated: 2020/12/13 16:21:06 by zminhas          ###   ########.fr       */
+/*   Updated: 2020/12/14 17:30:46 by zminhas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ char	*ft_strdup(const char *s1)
 	char	*dest;
 	int		i;
 
-	if (!(dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1))))
+	if (!(dest = (char *)ft_calloc(sizeof(char), (ft_strlen(s1) + 1))))
 		return (NULL);
 	i = -1;
 	while (s1[++i])
 		dest[i] = s1[i];
-	dest[i] = 0;
 	return (dest);
 }
